c53f168193ba76ea48a82d5f788024bca5593a4f
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-29 12:10:30 -0500

    Creacion Rutas Directas

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,10 @@
+const Pricing = () =>{
+    return(
+        <>
+        <h1>Princing</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, voluptas odio dignissimos recusandae ab provident voluptates? Tempore, eum eveniet velit, numquam aperiam delectus architecto quos est, quidem quisquam temporibus laboriosam.</p>
+        </>
+    )
+}
+
+export default Pricing
