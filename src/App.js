38ffcd120e2ab676950e556b3729ce97f2567f75
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-30 16:44:32 -0500

    Create routing and redirection

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Menu from './pages/Menu';
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import About from './pages/About';
+import P404 from './pages/P404';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="about" element={<About />} />
+        <Route path="*" element={<P404 />} />
       </Routes>
     </div>
   );
---
+++ src/pages/P404.jsx
@@ -0,0 +1,9 @@
+const P404 = () =>{
+    return(
+        <>
+        <h1>404</h1>
+        <p>page not found</p>
+        </>
+    )
+}
+export default P404
